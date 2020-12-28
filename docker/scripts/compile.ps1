84920d91eac6e77b863c6e9e3b265ab45229c11d
ggpwnkthx <ibjessup@gmail.com>
2020-12-28 17:30:27 -0500

    Windows Compiler
    Starting to refactor the code. Moving to all powershell scripts. Will be adding platform detection to build native binaries and allow for docker process isolation.

---
+++ docker/scripts/compile.ps1
@@ -0,0 +1,50 @@
+﻿param (
+    [string]$Branch = "master",
+    [string]$GitHubRepo = "TrinityCore/TrinityCore"
+)
+
+$Repo = Invoke-RestMethod -Method GET -Uri https://api.github.com/repos/$GitHubRepo
+$Branches = Invoke-RestMethod -Method GET -Uri https://api.github.com/repos/$GitHubRepo/branches
+$versions = [regex]::match($Repo.description, "[(].*[)]").Value
+$versions.Substring(1, $versions.Length -2).Split(",") | Foreach-Object {
+    $v = $_.Split("=")
+    $Branches | Where-Object -Property "name" -EQ $v[0].Trim() | Add-Member -MemberType NoteProperty -Name "version" -Value $v[1].Trim()
+}
+$version = ($Branches | Where-Object -Property "name" -EQ $Branch).Version
+
+# Create directories
+@(
+    "$PSScriptRoot/../../source/$version"
+    "$PSScriptRoot/../../builds/$version/db/base"
+    "$PSScriptRoot/../../clients/$version"
+) | Foreach-Object {
+    if (-not (Test-Path -Path $_)) {
+        New-Item -Path $_ -ItemType Directory | Out-Null
+    }
+}
+
+$DIR_Root = $PWD
+$DIR_Source = Resolve-Path -Path "$PSScriptRoot/../../source/$version"
+$DIR_Build = Resolve-Path -Path "$PSScriptRoot/../../builds/$version"
+$DIR_Client = Resolve-Path -Path "$PSScriptRoot/../../clients/$version"
+
+git clone -b $Branch $Repo.git_url $DIR_Source.Path
+cd $DIR_Source.Path
+git reset --hard
+git pull
+
+if ($Branch -eq "3.3.5") {
+    git pull --recurse-submodules https://github.com/ElunaLuaEngine/ElunaTrinityWotlk.git;
+    git submodule init;
+    git submodule update;
+    git remote add ElunaTrinityWotlk https://github.com/ElunaLuaEngine/ElunaTrinityWotlk.git;
+}
+
+cd $DIR_Root
+
+Switch ([environment]::OSVersion.Platform) {
+    "Win32NT" {
+        cmake @("-G","Visual Studio 16 2019","-S",$DIR_Source.Path,"-B",$DIR_Build.Path,"-DSCRIPTS=dynamic")
+        cmake @("--build",$DIR_Build.Path,"--config","Release")
+    }
+}
---
+++ scripts/compile.ps1
@@ -0,0 +1,71 @@
+﻿param (
+    [string]$branch = "master",
+    [string]$repo = "git://github.com/TrinityCore/TrinityCore.git"
+)
+if($branch -eq "master") {
+    $version = "9.0.2"
+} else {
+    $version = $branch
+}
+$SQL_ROOT_PW = "trinity_root"
+
+$SCRIPTROOT = "$PSScriptRoot/.."
+$SOURCE_DIR = "source/$version"
+$LOCAL_SOURCE_DIR = "$SCRIPTROOT\$SOURCE_DIR".Replace("/", "\")
+$BUILD_DIR = "build/$version"
+$LOCAL_BUILD_DIR = "$SCRIPTROOT\$BUILD_DIR".Replace("/", "\")
+$CLIENT_DIR = "clients/$version"
+$LOCAL_CLIENT_DIR = "$SCRIPTROOT\$CLIENT_DIR".Replace("/", "\")
+
+# Build compiler container
+echo "Building compiler environment image..."
+cd $SCRIPTROOT
+docker build -t trinitycore:windows -f .\docker\Dockerfile_windows .\docker\
+
+# Create directories
+if (-not (Test-Path -Path $LOCAL_SOURCE_DIR)) {
+    New-Item -Path $SCRIPTROOT -Name "source" -ItemType Directory | Out-Null
+    New-Item -Path $SCRIPTROOT/source -Name $version -ItemType Directory | Out-Null
+}
+if (-not (Test-Path -Path $LOCAL_BUILD_DIR)) {
+    New-Item -Path $SCRIPTROOT -Name "build" -ItemType Directory | Out-Null
+    New-Item -Path $SCRIPTROOT/build -Name $version -ItemType Directory | Out-Null
+    New-Item -Path $SCRIPTROOT/build/$version -Name "bin" -ItemType Directory | Out-Null
+    New-Item -Path $SCRIPTROOT/build/$version -Name "db" -ItemType Directory | Out-Null
+    New-Item -Path $SCRIPTROOT/build/$version/db -Name "base" -ItemType Directory | Out-Null
+}
+if (-not (Test-Path -Path $LOCAL_CLIENT_DIR)) {
+    New-Item -Path $SCRIPTROOT -Name "clients" -ItemType Directory | Out-Null
+    New-Item -Path $SCRIPTROOT/clients -Name $version -ItemType Directory | Out-Null
+}
+
+# Download source code
+docker run -it --rm `
+    -v $LOCAL_SOURCE_DIR/:C:/TrinityCore/ `
+    trinitycore:windows pwsh -c "
+        git clone -b $branch $repo C:\TrinityCore;
+        cd C:\trinitycore; 
+        git pull;
+    ".Replace("`r","")
+if($version -eq "3.3.5") {
+    # Download Eluna
+    docker run -it --rm `
+        -v $LOCAL_SOURCE_DIR/:C:/TrinityCore/ `
+        trinitycore:windows pwsh -c "
+            cd C:\TrinityCore;
+            git pull --recurse-submodules https://github.com/ElunaLuaEngine/ElunaTrinityWotlk.git;
+            git submodule init;
+            git submodule update;
+            git remote add ElunaTrinityWotlk https://github.com/ElunaLuaEngine/ElunaTrinityWotlk.git;
+        ".Replace("`r","")
+}
+
+# Build
+docker run -it --rm `
+    -v $LOCAL_SOURCE_DIR/:C:/TrinityCore/ `
+    -v $LOCAL_BUILD_DIR/:"C:/Program Files/TrinityCore/" `
+    trinitycore:windows pwsh -c "
+        & 'C:\Program Files\CMake\bin\cmake.exe' -G 'Visual Studio 16 2019' C:\TrinityCore\ -DCMAKE_INSTALL_PREFIX='C:\Program Files\TrinityCore' -DSCRIPTS=dynamic -DWITH_WARNINGS=1;
+        & 'C:\Program Files\CMake\bin\cmake.exe' --build . --config Release;
+        & 'C:\Program Files\CMake\bin\cmake.exe' --install . ;
+    ".Replace("`r","")
